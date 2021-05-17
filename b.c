8c6e3be7b1d0fd741478c1077e8d61378d779af5
mon <go_sa_ri@naver.com>
2021-05-17 16:20:33 +0900

    message

---
+++ b.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("This is a test file\n");
+	printf("Editing the file\n");
+
+	return 0;
+}
+
+
---
+++ hello.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "myheader.h"
+
+void printHello(void) {
+	printf("Hello World!\n");
+}
+
+
